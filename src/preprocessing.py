97875da1b7ebfeb5b5074a60bbf54f0c685a3f43
gomathiB2020 <gomathiseenu.s@gmail.com>
2026-03-24 11:20:43 +0530

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,13 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+def build_preprocessor(X):
+    num_cols = X.select_dtypes(include=["int64", "float64"]).columns
+    cat_cols = X.select_dtypes(include=["object", "category"]).columns
+
+    preprocessor = ColumnTransformer([
+        ("num", StandardScaler(), num_cols),
+        ("cat", OneHotEncoder(handle_unknown="ignore", sparse_output=False), cat_cols)
+    ])
+
+    return preprocessor
